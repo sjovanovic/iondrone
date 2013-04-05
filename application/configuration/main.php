3d685eb2d689860eb718a01e095e5967bb1c1a20
Slobodan Jovanovic <sjovanovic@gmail.com>
2013-04-05 09:36:20 -0700

    Adding new file: application/configuration/main.php

--- application/configuration/main.php
+++ application/configuration/main.php
@@ -1,8 +1,8 @@
 <?php
 $conf = array(
 	'dev'=>array(
-		'github_client_id' => 'afadb6b38e237828a7ee',
-		'github_secret'=>'4d1c6ec603bc62753c4624c230d77c268e52a6a6',
+      'github_client_id' => '', // add your github client id here
+      'github_secret'=>'', // add your github secret here
 		'path' => array(
 			'mvc_root'=> '../',
 			'model' => 'application/model',
@@ -15,8 +15,8 @@ $conf = array(
 		'site'=>isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on'?'https://'.$_SERVER['SERVER_NAME']:'http://'.$_SERVER['SERVER_NAME']
 	),
 	'beta'=>array(
-		'github_client_id' => 'afadb6b38e237828a7ee',
-		'github_secret'=>'4d1c6ec603bc62753c4624c230d77c268e52a6a6',
+		'github_client_id' => '',
+		'github_secret'=>'',
 		'path' => array(
 			'mvc_root'=> '../',
 			'model' => 'application/model',
@@ -29,8 +29,8 @@ $conf = array(
 		'site'=>isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']=='on'?'https://'.$_SERVER['SERVER_NAME']:'http://'.$_SERVER['SERVER_NAME']
 	),
 	'production'=>array(
-		'github_client_id' => 'afadb6b38e237828a7ee',
-		'github_secret'=>'4d1c6ec603bc62753c4624c230d77c268e52a6a6',
+		'github_client_id' => '',
+		'github_secret'=>'',
 		'path' => array(
 			'mvc_root'=> '../',
 			'model' => 'application/model',
